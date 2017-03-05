07d19c6b73138d3beee30dfa7e75c0b87d53f9aa
Juri Pakaste <juri@iki.fi>
2017-03-05 11:37:34 +0200

    Add a test for multiple parameter zip

--- PromiseTests/PromiseZipTests.swift
+++ PromiseTests/PromiseZipTests.swift
@@ -27,4 +27,27 @@ class PromiseZipTests: XCTestCase {
         XCTAssertEqual(tuple.0, 2)
         XCTAssertEqual(tuple.1, "some string")
     }
+
+    func testMultipleParameters() {
+        // The >2 parameter variants of zip are all generated, so testing one
+        // *should* mean all others work too.
+
+        weak var expectation = self.expectation(description: "`Promises.zip` should be type safe.")
+
+        let promise = Promise(value: 2)
+        let promise2 = Promise(value: "some string")
+        let promise3 = Promise(value: [1, 1, 2, 3, 5])
+        let promise4 = Promise(value: ["two", "strings"])
+        let zipped = Promises.zip(promise, promise2, promise3, promise4)
+        zipped.always({
+            expectation?.fulfill()
+        })
+
+        waitForExpectations(timeout: 1, handler: nil)
+        guard let tuple = zipped.value else { XCTFail(); return }
+        XCTAssertEqual(tuple.0, 2)
+        XCTAssertEqual(tuple.1, "some string")
+        XCTAssertEqual(tuple.2, [1, 1, 2, 3, 5])
+        XCTAssertEqual(tuple.3, ["two", "strings"])
+    }
 }
